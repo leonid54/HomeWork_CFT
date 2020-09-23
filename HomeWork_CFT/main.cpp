e9ae7b48e2ac1c8bbfc09c7b6538b0735c38424a
leonid54 <leongorbuncov3@gmail.com>
2020-09-23 21:44:50 +0700

    6 commit

--- HomeWork_CFT/main.cpp
+++ HomeWork_CFT/main.cpp
@@ -9,80 +9,24 @@ int main()
 	Data a;
 	string name, surname, DataBirth, password, RepeatPassword,answer,msg;
 	cout << "Регистрация нового пользователя:" <<endl
-		 <<"Введите Имя(Подсказка:Имя не должно содержать кириллицу):";
+		 <<"Введите Имя(Подсказка:Имя не должно содержать кириллицу,цифры):";
 	cin >> name;
 
-	while (!a.CheckSize(name))
-	{
-		cout << "Имя должно содержать более 1 буквы!(Подсказка:Имя не должно содержать кириллицу)" << endl;
-		cout << "Введите Имя:";
-		cin >> name;
-	}
-	while (!a.isStringAlpha(name))
-	{
-		cout << "Имя должно содержать только буквы!" << endl;
-		cout << "Введите Имя:";
-		cin >> name;
-	}
-	a.SetName(name);
-	cout << "Введите Фамилию(Подсказка:Фамилия не должна содержать кириллицу):";
+	cout << "Введите Фамилию(Подсказка:Фамилия не должна содержать кириллицу,цифры):";
 	cin >> surname;
-	while (!a.CheckSize(surname))
-	{
-		cout << "Фамилия должна содержать более 1 буквы!(Подсказка:Фамилия не должна содержать кириллицу)" << endl;
-		cout << "Введите Фамилию:";
-		cin >> surname;
-	}
-	while (!a.isStringAlpha(surname))
-	{
-		cout << "Фамилия должна содержать только буквы!" << endl;
-		cout << "Введите Фамилию:";
-		cin >> surname;
-	}
-	a.SetSurname(surname);
+	
 	cout << "Введите Дату Рождения(пример: 08.03.2001):";
 	cin >> DataBirth;
-	while (!a.CheckDataBirth(DataBirth))
-	{
-		cout << "Дата рождения должна содержать 10 чисел(пример: 08.03.2001)" << endl;
-		cout << "Введите Дату Рождения:";
-		cin >> DataBirth;
-	}
-	while (!a.isStringNumber(DataBirth))
-	{
-		cout << "Дата рождения должна содержать только цифры(пример: 08.03.2001)" << endl;
-		cout << "Введите Дату Рождения:";
-		cin >> DataBirth;
-	}
-	a.SetDataBirth(DataBirth);
+	
 	cout << "Придумайте Пароль(от 5 до 16 знаков):";
 	cin >> password;
-	while (!a.CheckPassword(password))
-	{
-		cout << "Пароль не может иметь меньше 5 и больше 15 знаков" << endl;
-		cout << "Придумайте Пароль:";
-		cin >> password;
-	}
-
+	
 	cout << "Подтвердите Пароль:";
 	cin >> RepeatPassword;
-	while (!a.CheckRepeatPassword(password, RepeatPassword))
-	{
-		cout << "Пароли не совпадают!" << endl;
-		cout << "Придумайте Пароль:";
-		cin >> password;
-		while (!a.CheckPassword(password))
-		{
-			cout << "Пароль не может иметь меньше 5 и больше 15 знаков" << endl;
-			cout << "Придумайте Пароль:";
-			cin >> password;
-		}
-		cout << "Подтвердите Пароль:";
-		cin >> RepeatPassword;
-	}
-	a.SetPassword(password);
+	
 	cout << "Регистрация(yes/no)?" << endl;
 	cin >> answer;
+
 	while(!a.CheckVolidatiton(answer,msg))
 	{
 		cout << msg << endl;
@@ -90,6 +34,66 @@ int main()
 	}
 	if (answer == "yes")
 	{
+		while (!a.CheckSize(name))
+		{
+			cout << "Имя должно содержать более 1 буквы!(Подсказка:Имя не должно содержать кириллицу)" << endl;
+			cout << "Введите Имя:";
+			cin >> name;
+		}
+		while (!a.isStringAlpha(name))
+		{
+			cout << "Имя должно содержать только буквы!" << endl;
+			cout << "Введите Имя:";
+			cin >> name;
+		}
+		a.SetName(name);
+		while (!a.CheckSize(surname))
+		{
+			cout << "Фамилия должна содержать более 1 буквы!(Подсказка:Фамилия не должна содержать кириллицу)" << endl;
+			cout << "Введите Фамилию:";
+			cin >> surname;
+		}
+		while (!a.isStringAlpha(surname))
+		{
+			cout << "Фамилия должна содержать только буквы!" << endl;
+			cout << "Введите Фамилию:";
+			cin >> surname;
+		}
+		a.SetSurname(surname);
+		while (!a.CheckDataBirth(DataBirth))
+		{
+			cout << "Дата рождения должна содержать 10 чисел(пример: 08.03.2001)" << endl;
+			cout << "Введите Дату Рождения:";
+			cin >> DataBirth;
+		}
+		while (!a.isStringNumber(DataBirth))
+		{
+			cout << "Дата рождения должна содержать только цифры(пример: 08.03.2001)" << endl;
+			cout << "Введите Дату Рождения:";
+			cin >> DataBirth;
+		}
+		a.SetDataBirth(DataBirth);
+		while (!a.CheckPassword(password))
+		{
+			cout << "Пароль не может иметь меньше 5 и больше 15 знаков" << endl;
+			cout << "Придумайте Пароль:";
+			cin >> password;
+		}
+		while (!a.CheckRepeatPassword(password, RepeatPassword))
+		{
+			cout << "Пароли не совпадают!" << endl;
+			cout << "Придумайте Пароль:";
+			cin >> password;
+			while (!a.CheckPassword(password))
+			{
+				cout << "Пароль не может иметь меньше 5 и больше 15 знаков" << endl;
+				cout << "Придумайте Пароль:";
+				cin >> password;
+			}
+			cout << "Подтвердите Пароль:";
+			cin >> RepeatPassword;
+		}
+		a.SetPassword(password);
 	    system("cls");
 	    cout << "Приветствие" << endl;
 	}
@@ -101,12 +105,15 @@ int main()
 	cin >> answer;
 	if (answer == "yes")
 	{
-		cout << "Добро пожаловать, " << name;
+		cout << "Добро пожаловать, " << name <<" "<<surname<<endl;
 	}
-	else
+	else if(answer == "no")
 	{
 		cout << "Отмена входа" << endl;
 	}
-	
+	else
+	{
+		cout << "Ошибка!Необходимо ввести 'yes' для продолжения, 'no' для выхода!" << endl;
+	}
 	return 0;
 }
